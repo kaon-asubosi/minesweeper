f5e5de14ae0b17bfff4b371e83ca60363bc410cb
taka <gamepley2022@gmail.com>
2025-05-28 22:42:31 +0900

    ボードサイズに合わせてサイズが全体の要素数が変更される部分を作れた、これから先に更にユーザーインプットに要素が入っていた場合処理する部分も作る

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,25 +17,49 @@ export default function Home() {
 
   const [userinput, setuserinput] = useState([]);
 
-  const [bomlist, setbomlist] = useState([]);
+  const [bombsmap, setbombsmap] = useState([]);
+
+  const [calctime, settime] = useState([]);
+
+  const [level, setlevel] = useState();
+
+  const [boardsize, setboardsize] = useState([9, 9]);
+
+  const [bombs_number, setnumber] = useState([]);
 
   const [sampleCounter, setsampleCounter] = useState(0);
 
   const sampleclickHandler = () => {
-    setsampleCounter((sampleCounter + 1) % 14);
+    setsampleCounter((sampleCounter + 1) % 15);
   };
-
   const clickHandler = (y: number, x: number) => {};
 
   const bomchecker = (input: number[], bomd: number[], size: number[]) => {};
 
+  const setboard = () => {
+    const calcboard: number[][] = Array.from({ length: boardsize[0] }, () =>
+      new Array<number>(boardsize[1]).fill(-1),
+    );
+    return calcboard;
+  };
+
   return (
     <div className={styles.container}>
-      <button
-        onClick={() => sampleclickHandler()}
-        className={styles.sampleCell}
-        style={{ backgroundPosition: `${-30 * sampleCounter}px` }}
-      />
+      <div
+        className={styles.board}
+        style={{ width: `${boardsize[0] * 30}px`, height: `${boardsize[1] * 30}` }}
+      >
+        {setboard().map((row, x) =>
+          row.map((i, y) => (
+            <button
+              key={'${x}-${y}'}
+              onClick={() => sampleclickHandler()}
+              className={styles.sampleCell}
+              style={{ backgroundPosition: `${-30 * i}px` }}
+            />
+          )),
+        )}
+      </div>
     </div>
   );
 }
