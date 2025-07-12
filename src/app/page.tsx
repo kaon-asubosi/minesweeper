66f41783c8e1afcaeae3be2e606c01af5390ac01
taka <gamepley2022@gmail.com>
2025-07-12 23:13:17 +0900

    クリックハンドラー部分を作った、次の次で完成する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
 
   const [boardSize, setBoardSize] = useState([9, 9]);
 
-  const [bombsNumber, setBombsNumber] = useState([]);
+  const [bombsNumber, setBombsNumber] = useState(10);
 
   const [sampleCounter, setSampleCounter] = useState(0);
 
@@ -36,20 +36,25 @@ export default function Home() {
   const setBoardLevel = (level: number) => {
     ///レベルを選択した時に起動する関数、ボードサイズを決め、セットインプットリストも起動する
     let Size: number[] = [];
+    let bomb: number = 0;
     if (level === 0) {
       setLevel('easy');
       Size = [9, 9];
+      bomb = 10;
     } else if (level === 1) {
       setLevel('normal');
       Size = [16, 16];
+      bomb = 40;
     } else if (level === 2) {
       setLevel('hard');
       Size = [16, 30];
+      bomb = 99;
     } else if (level === 3) {
       setLevel('custom');
     }
     setBoardSize(Size);
     setInputList(Size);
+    setBombsNumber(bomb);
   };
 
   const setInputList = (size: number[]) => {
@@ -61,16 +66,60 @@ export default function Home() {
   };
 
   const clickHandler = (y: number, x: number) => {
-    console.log(y, x);
+    const oneTimeInput = structuredClone(userInput);
+    ///まず爆弾被りのチェック、被っていたらゲームオーバー、被ってないならbombSearch起動して全部終わったら格納
+    ///初回チェック
+    if (userInput.flat().includes(1)) {
+      ///1が含まれていないので初回爆弾生成後爆弾ボード所有CheckAround
+    } else {
+      ///2回目以降なのでクリックチェックの後通常CheckAround
+      bombChecke([y, x]);
+      console.log(y, x);
+      return true;
+    }
   };
 
-  const bombChecke = (input: number[], bomb: number[], size: number[]) => {};
+  const bombChecke = (input: number[]) => {
+    ///2回目以降の処理爆弾と被っていないかを確認する
+  };
+  ///爆弾とクリックが被ってるかのチェック、初回（inputに1が無い時）だけ爆弾を生成する機能を持つ爆弾生成後は一回目は素通しする
 
-  const bombSearch = (input: number[], bomb: number[], size: number[]) => {};
+  const CheckAround = (input: number[], bombmap: number[][]) => {
+    ///周囲に爆弾があるかのチェックをする、周囲にある爆弾の数を返り値にする
+    let count = 0;
+    for (const direction of directions) {
+      const dy = direction[0] + input[0];
+      const dx = direction[1] + input[1];
+      if (bombsMap[dy] !== undefined && bombsMap[dy][dx] !== undefined && bombsMap[dy][dx] === 1) {
+        count += 1;
+      }
+    }
+    return count;
+  };
+
+  const bombSearch = (input: number[], bombmap: number[][], explored: number[][]) => {
+    ///自身から召喚される、周囲の爆弾を確認する機能と、周囲に無かった場合周りの8コマに自分を適応する機能を持つ
+    ///探索済みリストを使う、探索済みリスト受け取って探索に行き、探索済みリストを返す
+    const inNumber: number = CheckAround(input, bombmap);
+    let result: number[][] = explored.map((row) => [...row]);
+    ///-1が0,0が1を表すため-1
+    result[input[0]][input[1]] = inNumber - 1;
+    if (inNumber === 0) {
+      for (const direction of directions) {
+        const dy = direction[0] + input[0];
+        const dx = direction[1] + input[1];
+        if (result[dy] !== undefined && result[dy][dx] !== undefined && result[dy][dx] === -2)
+          ///枠外にズレていないかのチェックと既に探索しているかのチェックをここに挿入
+          result = bombSearch([dy, dx], bombmap, result).map((row) => [...row]);
+      }
+      ///0確認時拡散処理
+    }
+    return result;
+  };
 
   const board = (size: number[]) => {
     const calcBoard: number[][] = Array.from({ length: size[0] }, () =>
-      new Array<number>(size[1]).fill(-1),
+      new Array<number>(size[1]).fill(-2),
     );
     calcBoard[0][0] = 0;
     return calcBoard;
